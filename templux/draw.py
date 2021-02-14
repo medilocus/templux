f860d38ffe3ac4406e49ce33eee398aa6d51b0f0
HuangPatrick16777216 <huangpatrick16777216@gmail.com>
2021-02-14 13:59:55 -0800

    Draw wireframe.

---
+++ templux/draw.py
@@ -0,0 +1,32 @@
+#
+#  Graphic Videos
+#  An API for creating graphic videos in Python.
+#  Copyright Medilocus 2021
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from typing import Dict
+import pygame
+pygame.init()
+
+
+def draw_wire(data: Dict, thickness: int = 2):
+    surface = pygame.Surface(data["res"], pygame.SRCALPHA)
+    color = data["color"]
+
+    for line in data["lines"]:
+        pygame.draw.line(surface, color, *line, thickness)
+
+    return surface
